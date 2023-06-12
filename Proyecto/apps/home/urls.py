615c8bcc3a5cda48677dfa3166b61f71cffd5a0f
ClaraTortora <claratortora@gmail.com>
2023-06-12 16:25:29 -0300

    Update de urls de Proyecto

--- Proyecto/apps/home/urls.py
+++ Proyecto/apps/home/urls.py
@@ -1,6 +1,14 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login/", LoginView.as_view(template_name="home/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="home/logout.html"), name="logout"),
+    path("register/", views.register, name="register"),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
