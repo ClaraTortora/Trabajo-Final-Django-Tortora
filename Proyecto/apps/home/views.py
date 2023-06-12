aaff6479d64d2c113c447824d4081f22ad96b94b
ClaraTortora <claratortora@gmail.com>
2023-06-12 16:29:42 -0300

    Update de views: creando el login request y registro

--- Proyecto/apps/home/views.py
+++ Proyecto/apps/home/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
 
+#Importaciones para el login y el registro
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from . import forms
+
 # Create your views here.
 def index(request):
     return render (request, 'home/index.html')
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request.POST, data=request.POST)
+        if form.is_valid():
+            usuario = form.cleaned_data.get('username')
+            contraseña = form.cleaned_data.get('password')
+            user = authenticate(username=usuario, password=contraseña)
+            if user is not None:
+                login(request, user)
+                return render(request, 'home/index.html', {'mensaje':f'¡Bienvenid@ {usuario}!'})
+    else:
+        form = AuthenticationForm()
+    return render(request, 'home/login.html', {'form':form})
+
+def register(request):
+    if request.method == "POST":
+        form = forms.CustomUserCreationForm(request.POST)
+        if form.is_valid():
+           username = form.cleaned_data["username"]
+           form.save()
+           return render(request, 'home/index.html', {'mensaje':'¡Usuario creado con éxito!'})
+    else:
+        form = forms.CustomUserCreationForm()
+    return render(request, 'home/register.html', {'form':form})
+       
+           
