060ac026cd195b937f53e8bd6712f1bda295e53a
ClaraTortora <claratortora@gmail.com>
2023-06-12 16:27:02 -0300

    Registro de producto categoría admin

--- Proyecto/apps/producto/admin.py
+++ Proyecto/apps/producto/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+# admin.site.register(models.ProductoCategoria)
+
+@admin.register(models.ProductoCategoria)
+class ProductoCategoriaAdmin(admin.ModelAdmin):
+    list_display = ("nombre_producto", "descripcion_producto",)
+    search_fields = ("nombre_producto",)
+    list_filter = ("nombre_producto",)
+    ordering = ("nombre_producto",)
