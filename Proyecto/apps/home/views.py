bd8249e13e41a0af8ff80f4af8c2677baf145ffd
ClaraTortora <claratortora@gmail.com>
2023-06-13 23:13:23 -0300

    Update de views

--- Proyecto/apps/home/views.py
+++ Proyecto/apps/home/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 
 #Importaciones para el login y el registro
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from . import forms
+
+
 
 # Create your views here.
 def index(request):
     return render (request, 'home/index.html')
 
+# LOGIN
 def login_request(request):
     if request.method == "POST":
         form = AuthenticationForm(request.POST, data=request.POST)
@@ -21,19 +24,20 @@ def login_request(request):
                 return render(request, 'home/index.html', {'mensaje':f'¡Bienvenid@ {usuario}!'})
     else:
         form = AuthenticationForm()
-    return render(request, 'home/login.html', {'form':form})
+    return render(request, 'home/login.html', {'form': form})
 
+# REGISTRO
 def register(request):
     if request.method == "POST":
         form = forms.CustomUserCreationForm(request.POST)
         if form.is_valid():
-           username = form.cleaned_data["username"]
-           form.save()
-           return render(request, 'home/index.html', {'mensaje':'¡Usuario creado con éxito!'})
+            form.save()
+            return render(request, 'home/index.html', {'mensaje':'¡Usuario creado con éxito!'})
     else:
         form = forms.CustomUserCreationForm()
-    return render(request, 'home/register.html', {'form':form})
+    return render(request, 'home/register.html', {'form': form})
 
+# SOBRE MI 
 def aboutme(request):
     return render(request, 'home/aboutme.html')
     
