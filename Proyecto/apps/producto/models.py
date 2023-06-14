f2446186f52005d1eb5f75748c0fce7169f48ec5
ClaraTortora <claratortora@gmail.com>
2023-06-14 16:03:17 -0300

    Update: agregando verbose e image field

--- Proyecto/apps/producto/models.py
+++ Proyecto/apps/producto/models.py
@@ -4,7 +4,8 @@ from django.utils import timezone
 # Create your models here.
 class ProductoCategoria(models.Model):
     nombre_producto = models.CharField(max_length=50, unique=True)
-    descripcion_producto = models.CharField(max_length=200, null=True, blank=True)
+    descripcion_producto = models.CharField(max_length=200, null=True, blank=True, verbose_name='descripción')
+    fecha_de_actualizacion = models.DateTimeField(default=timezone.now, editable=False, verbose_name='Fecha de actualización')
     
     class Meta:
         verbose_name = 'Categoría de productos'
@@ -17,15 +18,28 @@ class ProductoCategoria(models.Model):
 class Producto(models.Model):
     
     categoria = models.ForeignKey(ProductoCategoria, on_delete=models.SET_NULL, blank=True, null=True, verbose_name='Categoria')
-    modelo = models.CharField(max_length=80)
+    modelo = models.CharField(max_length=40)
     precio = models.DecimalField(max_digits=15, decimal_places=2)
     stock = models.IntegerField()
     descripcion = models.CharField(max_length=200, null=True, blank=True, verbose_name='Descripción')
-    fecha_actualizacion = models.DateTimeField(default=timezone.now, editable=False, verbose_name='Fecha de actualización')
-    
+    fecha_de_publicacion = models.DateTimeField(default=timezone.now, editable=False, verbose_name='Fecha de publicación')
+    imagen_producto = models.ImageField(upload_to="imagenes" ,null=True, blank=True,)
+
+
     class Meta:
         verbose_name = "producto"
         verbose_name_plural = "productos"
 
     def __str__(self):
-        return f"{self.modelo} - ${self.precio:.2f}"
+        return f"{self.modelo} - ${self.precio:.2f} {self.imagen_producto}"
+    
+    
+class Ofertas(models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE, verbose_name='Producto')
+    precio = models.DecimalField(max_digits=15, decimal_places=2)
+    fecha_de_publicacion_oferta = models.DateTimeField(default=timezone.now, editable=False, verbose_name='Fecha de publicación de la oferta')
+    descripcion = models.CharField(max_length=200, null=True, blank=True, verbose_name='Descripción')
+    imagen_oferta = models.ImageField(null=True, blank=True, upload_to="celulares")
+    
+    def __str__(self):
+        return f"{self.producto} - ${self.precio:.2f} {self.imagen_oferta}"
