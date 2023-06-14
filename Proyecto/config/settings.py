3432822cd4ea4d23228b503089e2f539811655b4
ClaraTortora <claratortora@gmail.com>
2023-06-14 15:57:44 -0300

    Update de settings y urls: agregando imágenes

--- Proyecto/config/settings.py
+++ Proyecto/config/settings.py
@@ -112,6 +112,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+import os 
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
